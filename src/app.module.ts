5f1d617f5caacce6e4ddd550e3bbf3e02805f62a
naveed60 <advmiannaveedali@gmail.com>
2023-11-28 18:21:01 +0500

    admin module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { PostsModule } from './posts/posts.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [UsersModule, TasksModule, PostsModule],
+  imports: [UsersModule, TasksModule, PostsModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
