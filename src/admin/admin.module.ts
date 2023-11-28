e8922599a86274a00be4ea72720d8003d71916bb
naveed60 <advmiannaveedali@gmail.com>
2023-11-28 18:22:05 +0500

    admin services added

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
 
 @Module({
-  controllers: [AdminController]
+  controllers: [AdminController],
+  providers: [AdminService]
 })
 export class AdminModule {}
